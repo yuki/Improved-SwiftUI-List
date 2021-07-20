3f030444430504177c907df55d168ce548101c43
Yuki (aka Rubén Gómez) <rugoli@gmail.com>
2021-07-20 17:52:18 +0200

    Initial Commit

---
+++ ImprovedList/ImprovedListApp.swift
@@ -0,0 +1,17 @@
+//
+//  ImprovedListApp.swift
+//  ImprovedList
+//
+//  Created by Rubén Gómez Olivencia on 20/7/21.
+//
+
+import SwiftUI
+
+@main
+struct ImprovedListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
